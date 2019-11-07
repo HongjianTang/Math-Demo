584ad5d6dffe31624cf5808c8f3322a7f88c6368
Hongjian Tang <wanquanbuhuiyingwen@gmail.com>
2019-11-07 11:16:23 +0800

    [呈现待选数组](https://www.notion.so/9f43d3fd9d8e4d6593b614e5fed4caa9)

---
+++ Assets/Scripts/ProcessManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcessManager : MonoBehaviour
+{
+    private void Start()
+    {
+        Invoke(nameof(InitPhase), 1f);
+    }
+
+    void InitGame()
+    {
+        
+    }
+
+    void InitPhase()
+    {
+        NumberManager.Instance.ClearList();
+        NumberManager.Instance.GenerateRandomNumber();
+        UIManager.Instance.UpdateSelectNumber();
+    }
+
+    public void CheckPhase()
+    {
+        NumberManager.Instance.CheckArithmeticProgression();
+    }
+
+    void EndGame()
+    {
+        
+    }
+}
