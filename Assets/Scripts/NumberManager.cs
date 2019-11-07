e36719e1713e0f1adfb2ca3d3539343a9d3e9022
Hongjian Tang <wanquanbuhuiyingwen@gmail.com>
2019-11-07 10:28:17 +0800

    初始文件上传

---
+++ Assets/Scripts/NumberManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberManager : MonoBehaviour
+{
+    private List<int> _toSelectNumbers = new List<int>();
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public void GenerateSingleRandomNumber()
+    {
+        _toSelectNumbers.Add(Random.Range(1, 100));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void GenerateRandomNumber()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            GenerateSingleRandomNumber();
+        }
+    }
+
+    public void CheckAvailableArray()
+    {
+        
+    }
+}
