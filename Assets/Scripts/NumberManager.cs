b5e2481436973fad743d8d01263371f83b39ad3a
Hongjian Tang <wanquanbuhuiyingwen@gmail.com>
2019-11-07 10:48:38 +0800

    [随机出N个数字](https://www.notion.so/ee35c07252e44cab9e1a83c7a68e30a2)[数字列队判断是否有等差数列](https://www.notion.so/3cabf623168443e7a758e8513294b336)

--- Assets/Scripts/NumberManager.cs
+++ Assets/Scripts/NumberManager.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class NumberManager : MonoBehaviour
 {
     private List<int> _toSelectNumbers = new List<int>();
-    
+    private List<int> _playerNumbers = new List<int>();
+    private List<int> _progressionNumbers = new List<int>();
+    private int _score;
+    private int _scoreInterval;
+
     /// <summary>
-    /// 
+    /// 产生单个随机数
     /// </summary>
     public void GenerateSingleRandomNumber()
     {
@@ -15,7 +19,7 @@ public class NumberManager : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// 产生5个随机数
     /// </summary>
     public void GenerateRandomNumber()
     {
@@ -25,8 +29,85 @@ public class NumberManager : MonoBehaviour
         }
     }
 
-    public void CheckAvailableArray()
+    /// <summary>
+    /// 玩家选择数字
+    /// </summary>
+    /// <param name="n"></param>
+    public void SelectNumber(int n)
     {
-        
+        PlayerGetNumber(_toSelectNumbers[n]);
+    }
+
+    /// <summary>
+    /// 玩家获得数字
+    /// </summary>
+    /// <param name="n"></param>
+    public void PlayerGetNumber(int n)
+    {
+        _playerNumbers.Add(n);
+    }
+
+    /// <summary>
+    /// 检查是否存在等差数列
+    /// </summary>
+    public void CheckArithmeticProgression()
+    {
+        for (int i = 0; i < _playerNumbers.Count - 3; i++)
+        {
+            for (int j = i + 1; j < _playerNumbers.Count - 2; j++)
+            {
+                int n3 = 2 * _playerNumbers[j] - _playerNumbers[i];
+                for (int k = j + 1; k < _playerNumbers.Count - 1; k++)
+                {
+                    if (_playerNumbers[k] == n3)
+                    {
+                        int n4 = 2 * _playerNumbers[k] - _playerNumbers[j];
+                        for (int l = k + 1; l < _playerNumbers.Count; l++)
+                        {
+                            if (_playerNumbers[l] == n4)
+                            {
+                                _progressionNumbers.Add(_playerNumbers[i]);
+                                _progressionNumbers.Add(_playerNumbers[j]);
+                                _progressionNumbers.Add(_playerNumbers[k]);
+                                _progressionNumbers.Add(_playerNumbers[l]);
+                                _scoreInterval = l - i - 3;
+                                AddScoreAnimation();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void AddScoreAnimation()
+    {
+        AddScore(_scoreInterval);
+        RemoveNumber(_progressionNumbers[0]);
+        RemoveNumber(_progressionNumbers[1]);
+        RemoveNumber(_progressionNumbers[2]);
+        RemoveNumber(_progressionNumbers[3]);
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="n"></param>
+    public void RemoveNumber(int n)
+    {
+        _playerNumbers.Remove(n);
+        // 播放动画
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void AddScore(int n)
+    {
+        _score += n;
+        // 播放动画
     }
 }
